01c7fc7aa5794a141970c72623e3ee953e4b88e3
Training_H2A.09.22 <Training_h2a.09.22@accenture.com>
2018-02-15 09:34:25 +0530

    sample commit

---
+++ src/main/java/ProjectFeb14/SampleAppJenkins/SampleApp.java
@@ -0,0 +1,14 @@
+package ProjectFeb14.SampleAppJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is new standard ouput line");
+    }
+}
