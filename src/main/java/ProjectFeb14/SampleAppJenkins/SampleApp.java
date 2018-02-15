a3da94b04eb3839148fc9663f091b1c36df0df15
Training_H2A.09.22 <Training_h2a.09.22@accenture.com>
2018-02-15 09:38:32 +0530

    modified file name to resolve compilation error

--- src/main/java/ProjectFeb14/SampleAppJenkins/SampleApp.java
+++ src/main/java/ProjectFeb14/SampleAppJenkins/SampleApp.java
@@ -4,11 +4,10 @@ package ProjectFeb14.SampleAppJenkins;
  * Hello world!
  *
  */
-public class App 
+public class SampleApp 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         System.out.println("This is new standard ouput line");
     }
 }
